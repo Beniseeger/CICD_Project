019d93c612250c0c32bbd09c6dcee714e361e9b6
Anomatron37 <beniseeger98@gmail.com>
2022-07-30 20:12:28 +0200

    commiting files

--- udagram/set_env.sh
+++ udagram/set_env.sh
@@ -1,11 +1,11 @@
 # This file is used for convenience of local development.
 # DO NOT STORE YOUR CREDENTIALS INTO GIT
 export POSTGRES_USERNAME=postgres
-export POSTGRES_PASSWORD=myPassword
-export POSTGRES_HOST=mydbinstance.csxbuclmtj3c.us-east-1.rds.amazonaws.com
+export POSTGRES_PASSWORD=postgres
+export POSTGRES_HOST=cicd-udacity.c9mdowameroo.us-east-1.rds.amazonaws.com
 export POSTGRES_DB=postgres
-export AWS_BUCKET=arn:aws:s3:::myawsbucket-75139724085
+export AWS_BUCKET=arn:aws:s3:::cicd-frontend-s3bucket
 export AWS_REGION=us-east-1
-export AWS_PROFILE=default
+export AWS_PROFILE=udacity
 export JWT_SECRET=mysecretstring
-export URL=http://localhost:8100
+export URL=cicd-udacity.eba-225pgm2p.us-east-1.elasticbeanstalk.com
